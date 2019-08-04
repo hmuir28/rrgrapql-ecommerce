8790ef9090b8e6aa2f903802858b722187b6b14f
Harry Muir <harrymuir@ITs-MacBook-Pro-2.local>
2019-08-03 21:01:26 -0600

    added new action to add items into cart

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
+import CustomButton from "../custom-button/custom-button.component";
+import { addItem } from "../../redux/cart/cart.action";
+
+const CollectionItem = ({ item, addItem }) => (
   <div className="collection-item">
   <div 
     className="image"
     style={{
-      backgroundImage: `url(${imageUrl})`
+      backgroundImage: `url(${item.imageUrl})`
     }} />
   <div className="collection-footer">
-    <span className="name">{ name }</span>
-    <span className="price">${ price }</span>
+    <span className="name">{ item.name }</span>
+    <span className="price">${ item.price }</span>
   </div>
+  <CustomButton 
+    inverted
+    onClick={() => addItem(item)}> Agregar al Carrito </CustomButton>
 </div>
 )
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,14 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(c => c.id === cartItemToAdd.id);
+
+  if (existingCartItem) {
+    return cartItems.map(c => c.id === cartItemToAdd.id 
+      ?  {...c, quantity: c.quantity + 1}
+      : c)
+  }
+
+  return [...cartItems, {
+    ...cartItemToAdd, 
+    quantity: 1
+  }]
+};
